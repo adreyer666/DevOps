2ea4cc2736debb2b3be7bc7dafbe7541ab82d139
John Doe <jdoe@email.com>
2020-07-08 00:15:06 +0100

    make nicer

---
+++ Makefile
@@ -0,0 +1,23 @@
+#!/usr/bin/env make
+
+BUILD = default
+IMAGE = DevOps_$(BUILD)
+
+
+
+build:
+	vagrant up
+	vagrant ssh-config >> ~/.ssh/config
+	ssh -X $(BUILD)
+
+run:
+	virsh start $(IMAGE)
+	ssh -X $(BUILD)
+
+pause:
+	virsh managedsave $(IMAGE)
+
+clean:
+	virsh destroy $(IMAGE)
+	-vagrant destroy -f
+
--- install.sh
+++ install.sh
@@ -3,41 +3,75 @@
 DEBIAN_FRONTEND=noninteractive; export DEBIAN_FRONTEND
 
 apt-get update
-apt-get upgrade -y
+apt-get upgrade -q -y
 
-apt-get install -y --no-install-recommends \
-	curl ca-certificates gpg sudo \
-	xauth x11-apps xterm \
-	libxtst6 libasound2 \
-	fakeroot gpg-agent xdg-utils \
-	git jq \
-	build-essential \
-	pandoc fonts-dejavu
+apt-get install -q -y --no-install-recommends \
+    procps iproute2 \
+    curl ca-certificates gpg sudo \
+    lrzip iptables \
+    xauth x11-apps xterm \
+    libxtst6 libasound2 \
+    fakeroot gpg-agent xdg-utils \
+    git jq sshpass \
+    build-essential \
+    pandoc fonts-dejavu
 
-apt-get install -y --no-install-recommends \
+apt-get install -q -y --no-install-recommends \
     python3 python3-dev python3-pip python3-wheel \
-    python3-pytest pylint flake8 pyflakes3
+    python3-pytest pylint flake8 pyflakes3 python3-tk \
+    python3-setuptools \
+    python3-autopep8 \
+    python3-flake8 python3-flake8-docstrings \
+    python3-mccabe \
+    python3-pytest
+
+# add more utils and GUI apps
+apt-get install -q -y --no-install-recommends \
+    zsh meld gitk \
+    sqlitebrowser \
+    audacious firefox-esr
 
 # podman & kubernetes
-apt-get install -y --no-install-recommends \
+echo 'deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/Debian_Testing/ /' > /etc/apt/sources.list.d/libcontainers.list
+curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/Debian_Testing/Release.key | apt-key add -
+apt-get update
+apt-get install -q -y --no-install-recommends \
     ansible \
-    podman buildah umoci \
+    containers-storage \
+    podman buildah runc umoci slirp4netns tini \
     kubernetes-client kubetail
 
+# cleanup
+apt-get install -f
+apt-get clean && rm -rf /tmp/* /var/lib/apt/lists/* /var/cache/apt/archives/partial
+
+# ---- get/build some containers ---- #
+
+# activate user namespaces
+echo "kernel.unprivileged_userns_clone=1" >> /etc/sysctl.d/10-userns.conf
+sysctl -p /etc/sysctl.d/10-userns.conf
 
 mkdir -p /usr/local/src
-cd /usr/local/src
+
 # add x11docker to run X11 container apps
+cd /usr/local/src
 git clone https://github.com/mviereck/x11docker.git
 cp x11docker/x11docker /usr/local/bin
 chmod 755 /usr/local/bin/x11docker
 
-
 # add python IDE container
+cd /usr/local/src
 git clone https://github.com/adreyer666/Python-IDE.git
 cd Python-IDE
 make
 cp python-ide /usr/local/bin
 chmod 755 /usr/local/bin/python-ide
+
+# zsh
+mkdir /usr/local/src/ohmyzsh
+cd /usr/local/src/ohmyzsh
+curl -Lo install.sh https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+su - vagrant -c "/bin/sh /usr/local/src/ohmyzsh/install.sh --unattended"
+chsh -s /bin/zsh vagrant
 
 
